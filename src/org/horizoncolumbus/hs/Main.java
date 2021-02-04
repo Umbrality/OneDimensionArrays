5ca556efc74c4c089cd81631f7dce14262924712
HSA-Student <student.e.amofa@horiozncolumbus.org>
2021-02-04 08:45:44 -0500

    new stuff

--- src/org/horizoncolumbus/hs/Main.java
+++ src/org/horizoncolumbus/hs/Main.java
@@ -1,14 +1,22 @@
 package org.horizoncolumbus.hs;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        // declare the array and initializing
-        int[] numbers = new int[] {1, 2, 3, 4, 5};
+        // write your code here
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.print("Enter Array Size: ");
+
+        int ArraySize = myObj.nextInt();
+
+
+        int[] numbers = new int[ArraySize];
 
         //Array Literal
-     //   int[] numbers = new int[] {1, 2, 3, 4, 5};
+        //   int[] numbers = new int[] {1, 2, 3, 4, 5};
 
 //        numbers[0] = 1;
 //        numbers[1] = 2;
@@ -16,18 +24,22 @@ public class Main {
 //        numbers[3] = 4;
 //        numbers[4] = 5;
         //add data to the array
-        for (int i = 1; i < 5; i++){
-            numbers[i] = i + 1;
+        for (int i = 0; i < ArraySize; i++) {
+            System.out.print("Enter Array Number: ");
+            numbers[i] = myObj.nextInt();
         }
 
 
         //sum all the numbers in the array
         int sum = 0;
 
-         for(int i = 0; i < 5; i++){
-             sum = sum+numbers[i];
-         }
-        System.out.println(sum);
+        for (int i = 0; i < ArraySize; i++) {
+            sum = sum + numbers[i];
+        }
+
+            System.out.println(sum);
+
+        }
 
     }
-}
+
